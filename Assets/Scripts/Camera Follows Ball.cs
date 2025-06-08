2de4c941b09ce56a40b189eee5d5690af410866c
Shashank Sinha <shashanksinha590@gmail.com>
2025-06-08 16:45:32 +0530

    Added Script to make the camera follow the ball

---
+++ Assets/Scripts/Camera Follows Ball.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Camera Follows Ball script has started.");
+    }
+
+    // Update is called once per frame
+    public Vector3 distanceFromBall;
+    public Transform Ball;
+    void Update()
+    {
+        transform.position= Ball.position + distanceFromBall;
+    }
+}

9df4a97853fdd57bc767a4268265b24ca07fa20b
Shashank Sinha <shashanksinha590@gmail.com>
2025-06-10 12:49:08 +0530

    Added dummy coin collect animation

--- Assets/Scripts/Coin Collect Animation.cs
+++ Assets/Scripts/Coin Collect Animation.cs
@@ -5,12 +5,19 @@ public class CoinCollectAnimation : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Debug.Log("Coin Collect Animation Script Started");
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.gameObject.tag == "Ball")
+        {
+            Debug.Log("Coin collected!");
+            Invoke("RemoveCoin", 1f);
+        }
+    }
+    void RemoveCoin()
+    {
+        Destroy(gameObject);
     }
 }

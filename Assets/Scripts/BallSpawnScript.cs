acb1e3d09fcec94cf23906ca54050d905fefec12
Shashank Sinha <shashanksinha590@gmail.com>
2025-06-13 21:32:03 +0530

    Added camera to properly follow the ball prefab object on creation

--- Assets/Scripts/BallSpawnScript.cs
+++ Assets/Scripts/BallSpawnScript.cs
@@ -5,8 +5,10 @@ public class BallSpawnScript : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public GameObject ballPrefab;
     public Transform Position;
+    public CameraFollowsBall follow;
     void Start()
     {
-        Instantiate(ballPrefab, Position.position, Quaternion.identity);
+        GameObject ball= Instantiate(ballPrefab, Position.position, Quaternion.identity);
+        follow.Ball = ball.transform;
     }
 }

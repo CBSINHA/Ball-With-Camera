3194b690e77ac5532c1b7d3d590a7e801ce89f59
Shashank Sinha <shashanksinha590@gmail.com>
2025-06-18 12:43:27 +0530

    updates

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+    void Awake()
+    {
+        // Ensure that this object persists across scene loads
+        
+    }
+    int score = 0;
+    void(int point){
+}
